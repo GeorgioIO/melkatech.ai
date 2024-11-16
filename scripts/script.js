d0f99c8f6d2557cca48473b1c0e2cf923104867c
GeorgioIO <georgiojabbour.g.gj@gmail.com>
2024-11-16 10:42:03 +0200

    Work on testimonial part + js + make it responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,73 @@
+// TITLE : DOM ELEMENTS
 const burgerMenu = document.querySelector(".burger-menu");
 const CloseSidebarButton = document.querySelector(".close-sidebar");
+const switchers = document.querySelectorAll(".switch");
+console.log(switchers)
 
+// TITLE : EVENT LISTENERS
 burgerMenu.addEventListener("click", toggleSidebar);
 
+
+
+// -SUB EVENT LISTENERS- HANDLER TO CLOSE THE SIDEBAR
+CloseSidebarButton.addEventListener("click" , (event) => {
+    const sideBar = document.querySelector(".sidebar");
+
+    let sideBarDisplay = window.getComputedStyle(sideBar).display;
+
+    if(sideBarDisplay === "flex")
+    {
+        burgerMenu.addEventListener("click", toggleSidebar);
+        sideBar.style.display = "none";
+    }
+});
+
+// -SUB EVENT LISTENERS- HANDLERS FOR SWITCHERS TESTIMONIALS
+switchers.forEach(switcher => {
+    switcher.addEventListener("click" , () => {
+        removeSwitchActClass(event.target , switchers);
+        const testimonials = document.querySelectorAll(".testimonial-card")
+        toggleTestimonials(event.target , testimonials);
+
+    })
+});
+
+// TITLE : FUNCTIONS
+
+// -SUB FUNCTION- FUNCTION TO CHANGE BETWEEN TESTIMONIALS
+function toggleTestimonials(targetedSwitch , testimonials)
+{
+    const targetedValue = targetedSwitch.dataset.name;
+
+    testimonials.forEach(testimonial => {
+        if(testimonial.id === targetedValue)
+        {
+            testimonial.style.display = "block";
+        }
+        else
+        {
+            testimonial.style.display = "none";
+        }
+    });
+}
+// -SUB FUNCTIONS- FUNCTION TO REMOVE SWITCH-ACTIVE CLASS FROM NOT CLICKED BUTTONS
+function removeSwitchActClass (targetedSwitch , switchers)
+{
+    const targetedValue = targetedSwitch.dataset.name
+
+    switchers.forEach(switcher => {
+        if (targetedValue == switcher.dataset.name)
+        {
+            switcher.classList.add("switch-active");
+        }
+        else
+        {
+            switcher.classList.remove("switch-active");
+        } 
+    });
+}
+
+// -SUB FUNCTIONS- FUNCTION FOR THE BURGER MENU
 function toggleSidebar ()
 {
     const sideBar = document.querySelector(".sidebar");
@@ -19,16 +84,3 @@ function toggleSidebar ()
         sideBar.style.display = "flex";
     }
 }
-
-CloseSidebarButton.addEventListener("click" , (event) => {
-    const sideBar = document.querySelector(".sidebar");
-
-    let sideBarDisplay = window.getComputedStyle(sideBar).display;
-
-    if(sideBarDisplay === "flex")
-    {
-        burgerMenu.addEventListener("click", toggleSidebar);
-        sideBar.style.display = "none";
-    }
-    
-});
